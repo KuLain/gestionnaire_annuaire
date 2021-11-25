edfec74f0557d885964b373483412e480f45a3d0
Saimati <saimati.soufou@etu.univ-lyon1.fr>
2021-11-25 09:49:54 +0100

    ajout_client

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#include "header/struct/Record.h"
+#include "header/struct/AssociativeArray.h"
+
+void ajout_client(AARAY* tableau){
+
+char tab[7][];
+RECORD* tmp;
+
+fgets(tab,7,stdin);
+
+aapush(tableau,tab[0], tab[1],*record);
+
+}
+
+
 
 int main() {
     printf("Hello World\n");
