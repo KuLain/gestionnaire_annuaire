b8a3557ed75b1f6697ec5cbc30937e81ae381de2
p2103150 <sukirthan.linget@etu.univ-lyon1.fr>
2021-11-28 12:20:51 +0100

    Changement des fonctions de modification des fichiers pour directement écrire dessus après un appel de fonction

--- header/base_processing.h
+++ header/base_processing.h
@@ -5,5 +5,6 @@
 #ifndef SAE_1_2_BASE_PROCESSING_H
 #define SAE_1_2_BASE_PROCESSING_H
 #include "../header/struct/AssociativeArray.h"
-void add_record(AARRAY*);
+void add_record(AARRAY*, char[], char);
+void delete_record(AARRAY*, char[], char);
 #endif //SAE_1_2_BASE_PROCESSING_H
--- src/base_processing.c
+++ src/base_processing.c
@@ -3,15 +3,17 @@
 //
 
 #include "../header/base_processing.h"
+#include "../header/file_processing.h"
 #include <string.h>
 #include <stdlib.h>
 
-void add_record(AARRAY* array) {
+void add_record(AARRAY* array, char path[], char delim) {
     RECORD* client;
-    char **tmp = malloc(sizeof(char*));
+    FILE *fp;
+    char **tmp;
     int i, n;
 
-    printf("Entrez les informations du client\n");
+    printf("Entrez les informations du client à ajouter\n\n");
 
     for (i = 0; i < 7; i++) {
         switch (i) {
@@ -38,11 +40,69 @@ void add_record(AARRAY* array) {
                 break;
         }
         tmp[i] = malloc(sizeof(char)*150);
+        if (tmp[i] == NULL) {
+            perror("Error while allocating ");
+            exit(EXIT_FAILURE);
+        }
         fgets(tmp[i], 150, stdin);
         n = strlen(tmp[i]);
         tmp[i][n-1] = '\0';
-        tmp[i] = realloc(tmp[i], sizeof(char)*n);
+        tmp[i] = realloc(tmp[i], sizeof(char)*(n-1));
+        if (tmp[i] == NULL) {
+            perror("Error while reallocating ");
+            exit(EXIT_FAILURE);
+        }
     }
+    printf("\n");
+
     client = rinit(tmp);
     aapush(array, client->data[0], client->data[1], client);
+
+    fp = fopen(path, "a");
+    for (i = 0; i < 6; i++) {
+        fprintf(fp,"%s%c",client->data[i],delim);
+    }
+    fprintf(fp,"%s\n",client->data[6]);
+    fclose(fp);
+}
+
+void delete_record(AARRAY* array, char path[], char delim) {
+    char choix[3];
+    char prenom[50];
+    char nom[50];
+    char filtre[100];
+
+    printf("Entrez les informations du client à supprimer\n\n");
+    printf("Quels inforamtions voulez-vous utiliser ?\n");
+    printf("1) Prénom, nom et numéro de téléphone\n");
+    printf("2) Prénom, nom et adresse email\n\n");
+    printf("Entrez une valeur : ");
+    fgets(choix, 3, stdin);
+
+    printf("Entrez le prénom : ");
+    fgets(prenom, 50, stdin);
+    prenom[strlen(prenom)-1] = '\0';
+    printf("Entrez le nom : ");
+    fgets(nom, 50, stdin);
+    nom[strlen(nom)-1] = '\0';
+
+    switch (choix[0]) {
+        case '1':
+            printf("Entrez le numéro de téléphone : ");
+            fgets(filtre, 100, stdin);
+            filtre[strlen(filtre)-1] = '\0';
+            aapop(array, prenom, nom, NULL, filtre);
+            break;
+        case '2':
+            printf("Entrez l'adresse email : ");
+            fgets(filtre, 100, stdin);
+            filtre[strlen(filtre)-1] = '\0';
+            aapop(array, prenom, nom, filtre, NULL);
+            break;
+        default:
+            perror("Unknown input");
+            break;
+    }
+
+    aarray_csv(array, path, delim);
 }
