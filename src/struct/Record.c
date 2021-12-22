017adbccdc9b26dc72af25f994476bc366d75697
LINGET SUKIRTHAN p2103150 <sukirthan.linget@etu.univ-lyon1.fr>
2021-12-22 10:48:50 +0000

    Changement de l'affichage d'un RECORD

--- src/struct/Record.c
+++ src/struct/Record.c
@@ -31,8 +31,8 @@ RECORD* rinit(char **data)
 void rdisplay(RECORD *r)
 {
     if (r != NULL) {
-        // printf("%-25s%-30s%-25s%-15s%-20s%-50s%-20s\n", r->data[0], r->data[1], r->data[2], r->data[3], r->data[4], r->data[5], r->data[6]);
-        printf("%s %s %s %s %s %s %s\n", r->data[0], r->data[1], r->data[2], r->data[3], r->data[4], r->data[5], r->data[6]);
+        printf("%-25s%-30s%-25s%-15s%-20s%-50s%-20s\n", r->data[0], r->data[1], r->data[2], r->data[3], r->data[4], r->data[5], r->data[6]);
+        // printf("%s %s %s %s %s %s %s\n", r->data[0], r->data[1], r->data[2], r->data[3], r->data[4], r->data[5], r->data[6]);
     }
 }
 
