b85e7a4ceca9ca8c572329b6fa910eb2c2fa5ad2
Julien Linget <julien.linget@proton.me>
2022-06-20 21:19:06 +0200

    fix: SortWithCriteria
    fix: Sort with criteria
    
    
    fix: refactor Sort with Criteria

--- src/main/java/domain/application/SortWithCriteriaService.java
+++ src/main/java/domain/application/SortWithCriteriaService.java
@@ -4,6 +4,7 @@ import domain.model.Person;
 import infrastructure.repository.PersonRepository;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,97 +19,73 @@ public class SortWithCriteriaService {
     }
 
     public List<Person> sortWithCriteria(SortingCriteria criteria, Order order) {
+        List<Person> personList = null;
+
         switch (criteria) {
             case CITY -> {
-                return sortByCity(order);
+                personList = sortByCity();
             }
             case POSTAL_CODE -> {
-                return sortByPostalCode(order);
+                personList = sortByPostalCode();
             }
             case PHONE_NUMBER -> {
-                return sortByPhoneNumber(order);
+                personList = sortByPhoneNumber();
             }
             case EMAIL -> {
-                return sortByEmail(order);
+                personList = sortByEmail();
             }
             case JOB -> {
-                return sortByJob(order);
+                personList = sortByJob();
             }
         }
-        return null;
-    }
-
-    private List<Person> sortByCity(Order order) {
-        if (order == Order.ASC) {
-            return personRepository.getListPerson()
-                    .stream()
-                    .sorted(Comparator.comparing(p -> p.getLocation().getCity()))
-                    .collect(Collectors.toList());
-        } else {
-            return personRepository.getListPerson()
-                    .stream()
-                    .sorted(Comparator.comparing(p -> ((Person) p).getLocation().getCity()).reversed())
-                    .collect(Collectors.toList());
+        if (personList != null && order == Order.DESC) {
+            reverseList(personList);
         }
+
+        return personList;
     }
 
-    private List<Person> sortByPostalCode(Order order) {
-        if (order == Order.ASC) {
-            return personRepository.getListPerson()
-                    .stream()
-                    .sorted(Comparator.comparing(p -> p.getLocation().getPostalCode()))
-                    .collect(Collectors.toList());
-        } else {
-            return personRepository.getListPerson()
-                    .stream()
-                    .sorted(Comparator.comparing(p -> ((Person) p).getLocation().getPostalCode()).reversed())
-                    .sorted(Comparator.reverseOrder())
-                    .collect(Collectors.toList());
-        }
+    private List<Person> sortByCity() {
+        return personRepository.getListPerson()
+                .stream()
+                .sorted(Comparator.comparing(p -> p.getLocation().getCity()))
+                .collect(Collectors.toList());
     }
 
-    private List<Person> sortByPhoneNumber(Order order) {
-        if (order == Order.ASC) {
-            return personRepository.getListPerson()
-                    .stream()
-                    .sorted(Comparator.comparing(p -> p.getContact().getPhone()))
-                    .collect(Collectors.toList());
-        } else {
-            return personRepository.getListPerson()
-                    .stream()
-                    .sorted(Comparator.comparing(p -> ((Person) p).getContact().getPhone()).reversed())
-                    .sorted(Comparator.reverseOrder())
-                    .collect(Collectors.toList());
-        }
+    private List<Person> sortByPostalCode() {
+        return personRepository.getListPerson()
+                .stream()
+                .sorted(Comparator.comparing(p -> p.getLocation().getPostalCode()))
+                .collect(Collectors.toList());
     }
 
-    private List<Person> sortByEmail(Order order) {
-        if (order == Order.ASC) {
-            return personRepository.getListPerson()
-                    .stream()
-                    .sorted(Comparator.comparing(p -> p.getContact().getEmailAdress()))
-                    .collect(Collectors.toList());
-        } else {
-            return personRepository.getListPerson()
-                    .stream()
-                    .sorted(Comparator.comparing(p -> ((Person) p).getContact().getEmailAdress()).reversed())
-                    .sorted(Comparator.reverseOrder())
-                    .collect(Collectors.toList());
-        }
+    private List<Person> sortByPhoneNumber() {
+        return personRepository.getListPerson()
+                .stream()
+                .sorted(Comparator.comparing(p -> p.getContact().getPhone()))
+                .collect(Collectors.toList());
     }
 
-    private List<Person> sortByJob(Order order) {
-        if (order == Order.ASC) {
-            return personRepository.getListPerson()
-                    .stream()
-                    .sorted(Comparator.comparing(Person::getJob))
-                    .collect(Collectors.toList());
-        } else {
-            return personRepository.getListPerson()
-                    .stream()
-                    .sorted(Comparator.comparing(Person::getJob))
-                    .sorted(Comparator.reverseOrder())
-                    .collect(Collectors.toList());
+    private List<Person> sortByEmail() {
+        return personRepository.getListPerson()
+                .stream()
+                .sorted(Comparator.comparing(p -> p.getContact().getEmailAdress()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Person> sortByJob() {
+        return personRepository.getListPerson()
+                .stream()
+                .sorted(Comparator.comparing(Person::getJob))
+                .collect(Collectors.toList());
+    }
+
+    private void reverseList(List<Person> listToReverse) {
+        for (int i = 0; i < listToReverse.size()/2; i++) {
+            int endIndex = listToReverse.size()-1-i;
+            Person tmp = listToReverse.get(endIndex);
+            listToReverse.set(endIndex, listToReverse.get(i));
+            listToReverse.set(i, tmp);
         }
     }
 }
